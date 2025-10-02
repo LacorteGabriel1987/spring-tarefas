9b49cbc6e62a6146da0c01a24e1f9355efd01dcc
GabrielLacorte <gabrielslacorte@hotmail.com>
2025-10-02 20:58:18 -0300

    cria TarefaService e TarefaController com endpoints CRUD

--- tarefas/src/main/java/com/gabriel/tarefas/service/TarefaService.java
+++ tarefas/src/main/java/com/gabriel/tarefas/service/TarefaService.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class TarefaService {
     private final TarefaRepository tarefaRepository;
 
@@ -16,9 +18,39 @@ public class TarefaService {
         this.tarefaRepository = tarefaRepository;
     }
 
+    //Listar todos
     public List<Tarefa> buscarTarefas(){
         return tarefaRepository.findAll();
     }
+    //Buscar por ID
+    public Optional<Tarefa> buscarPorId(Long id){
+        return tarefaRepository.findById(id);
+    }
 
+    //Salvar
+    public Tarefa salvarTarefa(Tarefa tarefa){
+        return tarefaRepository.save(tarefa);
+    }
 
+    //Atualizar
+    public  Tarefa atualizarTarefa(Long id, Tarefa tarefaAtualizada){
+        return tarefaRepository.findById(id)
+                .map(tarefa ->{
+                    tarefa.setTitulo(tarefaAtualizada.getTitulo());
+                    tarefa.setDescricao(tarefaAtualizada.getDescricao());
+                    tarefa.setStatus(tarefaAtualizada.getStatus());
+                    tarefa.setDataLimite(tarefaAtualizada.getDataLimite());
+
+                    return tarefaRepository.save(tarefa);
+                })
+                .orElseThrow(()-> new RuntimeException("Tarefa não existe"));
+    }
+    //Deletar
+    public void deletarTarefa(Long id) {
+        if (tarefaRepository.existsById(id)) {
+            tarefaRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Tarefa não encontrada");
+        }
+    }
 }
